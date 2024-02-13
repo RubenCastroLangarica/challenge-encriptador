cd639a514c2035126e6c9d8127a5201351159345
Ruben <rubencastrolangarica@gmail.com>
2024-02-13 10:55:53 -0600

    Agregar alerta

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function encriptar(){
         muñeco.src = "./img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        alert("Debes ingresar algún texto");
+        swal ("Atención", "Debes ingresar un texto", "warning");
     }
 }
 
@@ -56,7 +56,7 @@ function desencriptar(){
         muñeco.src = "./img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        alert("Debes ingresar algún texto"); 
+        swal ("Atención", "Debes ingresar un texto", "warning"); 
     }
 
     
